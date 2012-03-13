a95b9e7870a96914d9552fb0cd1087903c190e34
Ryan Muller <cognitionmachine@gmail.com>
2012-03-13 16:09:33 -0400

    memories controller, not yet working

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  acts_as_learner
+
+  def learn(item)
+    Spaceable::Memory.create(:component_id => item.id, :learner_id => self.id)
+  end
+
+  def studying?(item)
+    self.memories.find_by_component_id(item.id)
+  end
 end
