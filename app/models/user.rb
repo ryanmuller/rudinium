b8d2a4de93b62cc39b0728ba80ac267864b75f09
Ryan Muller <cognitionmachine@gmail.com>
2012-03-18 21:38:59 -0400

    added controller and user methods for handling facebook callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,20 @@ class User < ActiveRecord::Base
     Item.joins(:memories).where('memories.learner_id' => self.id)
   end
 
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token.extra.raw_info
+    if user = User.where(:email => data.email).first
+      user
+    else # Create a user with a stub password. 
+      User.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+      end
+    end
+  end
 end
