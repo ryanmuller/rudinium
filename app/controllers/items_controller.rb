3121f21fe4cd30cf0923adc6deb232d168f935db
cicatriz <ryan@geknowm.com>
2014-03-23 11:53:57 -0500

    order quizzes by id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.includes(:memories, :quizzes).all # might be able to ditch "includes"
     @lectures = Lecture.all
-    @quizzes = Quiz.all
+    @quizzes = Quiz.order(:id)
 
     # creates JSON array of user's memories to pass to backbone.js app
     @memories = current_user.nil? ? [] : current_user.memories.map{|m| {:id => m.id, :due => m.due?, :item_id => m.component_id, :quizzes => m.component.quizzes.map{|q| q.id}}}
